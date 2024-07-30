edf38d7df75bb341f022186db37d278683beddfe
Hamza <144457566+unk-user@users.noreply.github.com>
2024-07-30 16:05:28 +0100

    add validateRequest

--- auth.ts
+++ auth.ts
@@ -2,6 +2,9 @@ import { Lucia, TimeSpan } from 'lucia';
 
 import { PrismaAdapter } from '@lucia-auth/adapter-prisma';
 import prisma from './lib/db';
+import { User, Session } from '@prisma/client';
+import { cookies } from 'next/headers';
+import { cache } from 'react';
 
 const adapter = new PrismaAdapter(prisma.session, prisma.user);
 
@@ -21,6 +24,45 @@ export const lucia = new Lucia(adapter, {
     };
   },
 });
+
+export const uncachedValidateRequest = async (): Promise<
+  { user: User; session: Session } | { user: null; session: null }
+> => {
+  const sessionId = cookies().get(lucia.sessionCookieName)?.value ?? null;
+  if (!sessionId) {
+    return {
+      user: null,
+      session: null,
+    };
+  }
+
+  const result = await lucia.validateSession(sessionId);
+
+  try {
+    if (result.session && result.session.fresh) {
+      const sessionCookie = lucia.createSessionCookie(result.session.id);
+      cookies().set(
+        sessionCookie.name,
+        sessionCookie.value,
+        sessionCookie.attributes
+      );
+    }
+    if (!result.session) {
+      const sessionCookie = lucia.createBlankSessionCookie();
+      cookies().set(
+        sessionCookie.name,
+        sessionCookie.value,
+        sessionCookie.attributes
+      );
+    }
+  } catch {
+    console.error('Failed to set session cookie');
+  }
+
+  return result;
+};
+
+export const validateRequest = cache(uncachedValidateRequest);
 
 declare module 'lucia' {
   interface Register {
